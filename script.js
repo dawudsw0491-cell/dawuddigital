1ba59165c48e8c615c432371c36ac5674e55c5db
dawudsw0491-cell <dawudsw0491@gmail.com>
2026-06-13 14:02:59 +0700

    Add scroll animation

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("Dawud Digital");
+document.addEventListener("DOMContentLoaded", () => {
+
+const sections = document.querySelectorAll(".section");
+
+const observer = new IntersectionObserver(
+(entries) => {
+
+entries.forEach(entry => {
+
+if(entry.isIntersecting){
+
+entry.target.style.opacity = "1";
+entry.target.style.transform = "translateY(0)";
+
+}
+
+});
+
+},
+{
+threshold:0.1
+}
+);
+
+sections.forEach(section => {
+
+section.style.opacity = "0";
+section.style.transform = "translateY(40px)";
+section.style.transition = "all 0.8s ease";
+
+observer.observe(section);
+
+});
+
+});
